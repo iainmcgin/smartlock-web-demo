3ad084ea48cf1e307d2f9fa6304125e738da9bb2
Iain McGinniss <iainmcgin@google.com>
2017-08-24 13:08:48 -0700

    Fix cancel / error handling

--- login.js
+++ login.js
@@ -2,8 +2,8 @@
 const clientId =
     '114694971460-q4jm7hi4tj9ph2v1ohlql42oome73suk.apps.googleusercontent.com';
 
-// The parameters used for hint and retrieve requests. Essentially, we are 
-// asking for a Google account, and providing the information necessary to 
+// The parameters used for hint and retrieve requests. Essentially, we are
+// asking for a Google account, and providing the information necessary to
 // source an ID token which is correctly scoped to this application.
 const credentialOptions = {
   supportedAuthMethods: ['https://accounts.google.com'],
@@ -27,14 +27,24 @@ function onLoad() {
     goToMain();
     return;
   }
-
-  window.setTimeout(tryAuth, 1000);
 }
 
 function tryAuth() {
   // attempt to retrieve an existing account.
   smartlock.retrieve(credentialOptions)
       .then(handleSmartlockResult)
+      .catch((smartLockError) => {
+        switch (smartLockError.type) {
+          case 'noCredentialsAvailable':
+            console.log('No existing credentials - will try hint retrieval');
+            return false;
+          case 'userCanceled':
+            console.log('User canceled retrieve - will try manual auth');
+            return true;
+          default:
+            return true;
+        }
+      })
       .then((complete) => {
         if (!complete) {
           // we were unable to retrieve an existing credential, so ask for
@@ -45,9 +55,9 @@ function tryAuth() {
         return true;
       })
       .catch((err) => {
-        console.log('retrieve request failed :(');
-        handleSmartlockResult(null);
+        console.log('hint request failed', err);
       });
 }
 
+window.onSmartLockLoad = tryAuth;
 window.addEventListener('load', onLoad);
